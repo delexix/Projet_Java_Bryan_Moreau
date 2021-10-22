dc612087a0bc335f4bac1ba49560061dc45aa7fd
delexix <61683204+delexix@users.noreply.github.com>
2021-10-22 17:29:19 +0200

    Update ThreadAvancementCommande.java

--- src/main/java/systeme/ThreadAvancementCommande.java
+++ src/main/java/systeme/ThreadAvancementCommande.java
@@ -4,12 +4,17 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.util.Duration;
 import javafx.scene.control.Alert.AlertType;
 
 public class ThreadAvancementCommande extends Thread {
@@ -28,28 +33,33 @@ public class ThreadAvancementCommande extends Thread {
 		}
 		produits.addAll(commande.getProduits());
 		
-		ExecutorService service = Executors.newFixedThreadPool(5);
-		
 		//simule le temps de préparation des produits
 		for(int j = 0; j<produits.size();j++) {
-			try {
-				Thread.sleep(TimeUnit.SECONDS.toMillis((produits.get(j).calculTemps()).longValue()));
-				(new Alert(AlertType.INFORMATION,"le "+produits.get(j).getNom()+" de la commande de "+commande.getClient().getPrenom()+" "+commande.getClient().getNom()+" est terminé.",ButtonType.OK)).show();
-			} catch (InterruptedException e) {
-				
-				e.printStackTrace();
-			}
+			final int index = j;
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+						@Override
+						public void run() {
+							(new Alert(AlertType.INFORMATION,"le "+produits.get(index).getNom()+" de la commande de "+commande.getClient().getPrenom()+" "+commande.getClient().getNom()+" est terminé.",ButtonType.OK)).show();							
+						}
+						
+					},TimeUnit.SECONDS.toMillis((produits.get(j).calculTemps()).longValue()));
 			
 		}
-		
-		(new Alert(AlertType.INFORMATION,"La commande de "+commande.getClient().getPrenom()+" "+commande.getClient().getNom()+" est terminé.",ButtonType.OK)).show();
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+					@Override
+					public void run() {
+						(new Alert(AlertType.INFORMATION,"La commande de "+commande.getClient().getPrenom()+" "+commande.getClient().getNom()+" est terminé.",ButtonType.OK)).show();
 
-		//met la commande en terminé
-		commande.nextStatut();
+						//met la commande en terminé
+						commande.nextStatut();
+						
+						//enregistrement de la commande dans un historique
+						FileJsoner<HistoriqueCommande> jsoner = new FileJsoner<HistoriqueCommande>("./src/main/resources/"+commande.getClient().getNumero()+".json");
+						jsoner.writeToFileNotOverwrite(new HistoriqueCommande(Date.from(Instant.now()),commande.getPrix(),commande.getMenus(),commande.getProduits()));					}
+					
+				},TimeUnit.SECONDS.toMillis((commande.calculTemps()).longValue()));
 		
-		service.shutdown();
-		//enregistrement de la commande dans un historique
-		FileJsoner<HistoriqueCommande> jsoner = new FileJsoner<HistoriqueCommande>("./src/main/resources/"+commande.getClient().getNumero()+".json");
-		jsoner.writeToFileNotOverwrite(new HistoriqueCommande(Date.from(Instant.now()),commande.getPrix(),commande.getMenus(),commande.getProduits()));
 	}
 }
