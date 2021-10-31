422cf7a5cea1b2b2b525fc9d40cda4a2ba445519
delexix <61683204+delexix@users.noreply.github.com>
2021-10-31 23:13:28 +0100

    Update BorneCommande.java

--- src/main/java/systeme/BorneCommande.java
+++ src/main/java/systeme/BorneCommande.java
@@ -1,20 +1,23 @@
 package systeme;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.Scanner;
 
 public class BorneCommande {
 	private DataBase data;
 
-	//constructeur de la borne de commande
+	/**
+	 * constructeur de la borne de commande
+	 */
 	public BorneCommande() {
 		super();
 		this.data= new DataBase();
 	}
 	
-	//permet de trouver le client grâce à son numero, s'il n'est pas trouver un Optional empty est renvoyé
+	/**
+	 * permet de trouver le client grâce à son numero, s'il n'est pas trouver un Optional empty est renvoyé
+	 * @param numero : numero de client
+	 * @return Optional<Client> : classe permetant de ne pas avoir de null, si l'utilisateur a mis le bon numéro le Optional aura une valeur
+	 */
 	public Optional<Client> identifier(Integer numero) {
 		boolean trouver = false;
 		int i=0;
@@ -29,7 +32,11 @@ public class BorneCommande {
 		return Optional.ofNullable(c);
 	}
 	
-	//trouve le menu selon son nom
+	/**
+	 * trouve le menu selon son nom
+	 * @param value : nom du menu recherché
+	 * @return Menu : menu recherché
+	 */
 	public Menu findMenu(String value) {
 		int i=0;
 		boolean trouver = false;
@@ -44,7 +51,11 @@ public class BorneCommande {
 		return menu;
 	}
 	
-	//trouve le produit selon son nom
+	/**
+	 * trouve le produit selon son nom
+	 * @param value : nom du produit recherché
+	 * @return Produit : produit recherché
+	 */
 	public Produit findProduit(String value) {
 		int i=0;
 		boolean trouver = false;
